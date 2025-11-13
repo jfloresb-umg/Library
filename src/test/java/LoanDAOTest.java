730ae1620f76c56ce625c94a2935e47dbbe8e0c3
JAFB <jfloresb9@miumg.edu.gt>
2025-11-13 17:27:55 -0600

    Update All

--- src/test/java/LoanDAOTest.java
+++ src/test/java/LoanDAOTest.java
@@ -1,12 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import com.library.dao.LoanDAO;
+import com.library.model.Loan;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
 
 /**
  *
  * @author josef
  */
 public class LoanDAOTest {
-    
+
+    private static LoanDAO loanDAO;
+
+    @BeforeAll
+    static void setup() {
+        loanDAO = new LoanDAO();
+    }
+
+    @Test
+    public void testAddLoan() {
+        Loan loan = new Loan(1, 1, LocalDate.now().plusDays(7));
+        loanDAO.addLoan(loan);
+
+        Loan result = loanDAO.getLoanById(1);
+        assertNotNull(result);
+        assertEquals(1, result.getUserId());
+    }
+
+    @Test
+    public void testUpdateReturn() {
+        loanDAO.updateReturn(1, 5.0);
+        Loan result = loanDAO.getLoanById(1);
+        assertEquals(5.0, result.getFine());
+        assertNotNull(result.getReturnDate());
+    }
 }
